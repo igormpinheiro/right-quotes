58d99f5c46d315cd54bfb086edd30909f46a358c
Igor Pinheiro <igor@PC-IGOR-1223>
2024-08-14 13:50:10 -0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="quote-container">
+        <p class="quote-text">"A única coisa necessária para o triunfo do mal é que os homens bons não façam nada."</p>
+        <p class="quote-author">- Edmund Burke</p>
+    </div>
+  </div>
+</template>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            padding: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .quote-container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+            text-align: center;
+        }
+        .quote-text {
+            font-size: 24px;
+            font-style: italic;
+            margin-bottom: 15px;
+        }
+        .quote-author {
+            font-size: 20px;
+            font-weight: bold;
+            color: #555;
+        }
+    </style>
